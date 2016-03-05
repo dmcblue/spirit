c533a25ce7d33ae5224a27d671e8cfb1f40e855a
daniel.crawford <daniel.crawford@audionow.com>
2016-03-05 13:22:10 -0500

    Tumblr share

--- web/js/spirit.js
+++ web/js/spirit.js
@@ -218,7 +218,7 @@ var spirit = spirit || (function(){
 													window.open(
 														makeTumblrLink(
 															link('embed', true), 
-															makeText('<br/><br/>'), 
+															makeText('<p>&nbsp;</p>'), 
 															makeCitation()
 																+ ' from <a target="_blank" href="' 
 																+ link('embed', true)
